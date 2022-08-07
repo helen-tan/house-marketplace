ced6dde01e559ef7000eab538fa23bc1258be483
helen tan <helentanmz0909@gmail.com>
2022-08-07 12:10:56 +0800

    useAuthStatus custom hook: check if user is logged in

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,20 @@
+import { useEffect, useState, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [checkingStatus, setCheckingStatus] = useState(true) // can also be 'loading'
+
+  useEffect(() => {
+    const auth = getAuth()
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true)
+      }
+      setCheckingStatus(false)
+    })
+  })
+
+  return { loggedIn, checkingStatus }
+}
