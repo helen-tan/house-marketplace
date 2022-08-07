7c44be82f18ae6345a1a0b6c08bca74dbca37d15
helen tan <helentanmz0909@gmail.com>
2022-08-07 12:19:50 +0800

    Prevent memory error leak: useRef & useEffect cleanup function

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -4,17 +4,25 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false)
   const [checkingStatus, setCheckingStatus] = useState(true) // can also be 'loading'
+  const isMounted = useRef(true)
 
   useEffect(() => {
-    const auth = getAuth()
+    // prevent state from being updated id component is unmounted (memory leak error)
+    if(isMounted) {
+      const auth = getAuth()
 
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true)
-      }
-      setCheckingStatus(false)
-    })
-  })
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true)
+        }
+        setCheckingStatus(false)
+      })
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
 
   return { loggedIn, checkingStatus }
 }
